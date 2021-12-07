5830219cf80319796bdf99a216ee32cf02d023f9
RenanSOkada <renanokada2000@gmail.com>
2021-12-07 13:41:46 -0400

    Finalizando a primeira parte! Com a finalização do PUT e DELETE!

--- productRest/src/main/java/com/compasso/productRest/controller/ProdutosController.java
+++ productRest/src/main/java/com/compasso/productRest/controller/ProdutosController.java
@@ -2,12 +2,14 @@ package com.compasso.productRest.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.compasso.productRest.controller.dto.DetalhesProdutoDto;
 import com.compasso.productRest.controller.dto.ProdutoDto;
-import com.compasso.productRest.controller.form.AtualizacaoProdutoForm;
 import com.compasso.productRest.form.ProdutoForm;
 import com.compasso.productRest.modelo.Produto;
 import com.compasso.productRest.repository.ProdutoRepository;
@@ -47,6 +47,7 @@ public class ProdutosController {
 	}
 
 	@PostMapping
+	@Transactional
 	public ResponseEntity<ProdutoDto> cadastrar(@RequestBody @Valid ProdutoForm form, UriComponentsBuilder uriBuilder) {
 		Produto produto = form.converter();
 		produtoRepository.save(produto);
@@ -56,17 +57,37 @@ public class ProdutosController {
 	}
 
 	@GetMapping("/{id}")
-	public ProdutoDto detalhar(@PathVariable Long id) {
-		Produto produto = produtoRepository.getById(id);
-		return new ProdutoDto(produto);
+	public ResponseEntity<ProdutoDto> detalhar(@PathVariable Long id) {
+		Optional<Produto> optional = produtoRepository.findById(id);
+		if (optional.isPresent()) {
+			return ResponseEntity.ok(new ProdutoDto(optional.get()));
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 
 	@PutMapping("/{id}")
 	@Transactional
 	public ResponseEntity<ProdutoDto> atualizat(@PathVariable Long id, @RequestBody @Valid ProdutoForm form) {
-		Produto produto = form.atualizar(id, produtoRepository);
 
-		return ResponseEntity.ok(new ProdutoDto(produto));
+		Optional<Produto> optional = produtoRepository.findById(id);
+		if (optional.isPresent()) {
+			Produto produto = form.atualizar(id, produtoRepository);
+			return ResponseEntity.ok(new ProdutoDto(produto));
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> excluir(@PathVariable Long id) {
+
+		Optional<Produto> optional = produtoRepository.findById(id);
+		if (optional.isPresent()) {
+			produtoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
 
 	}
 
